cbc483122074fe99ec36be9dfc1c8df87c3fb8bc
Dylan Green <dylan.green61@gmail.com>
2019-11-10 16:37:26 -0800

    mock data

---
+++ insert_data/ubr_ins_customer.sql
@@ -0,0 +1,6 @@
+insert into customer values (5551234, 'John', '222 Strawberry Fields', 'Leicester', 02938, 0);
+insert into customer values (5552331, 'Paul', '15 Abbey Road', 'London', 56731, 0);
+insert into customer values (5552511, 'Ringo', '251 Manchester Way', 'Liverpool', 85847, 0);
+insert into customer values (5551342, 'George', '8 Maxwell Drive', 'Sussex', 47292, 0);
+
+commit customer;
