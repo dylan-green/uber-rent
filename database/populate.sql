16c47e5a834397d2fb842e3768dc3c9a5ac97aed
UBC Student <sstarke@valdes.students.cs.ubc.ca>
2019-11-21 14:16:16 -0800

    removed member/points attribute from customer

--- database/populate.sql
+++ database/populate.sql
@@ -11,18 +11,18 @@ INSERT INTO branch VALUES ('77 Rokewood Avenue','Stellenbosch');
 
 
 /* Populate the Customer table */
-INSERT INTO customer VALUES (5551234, 'John', '222 Strawberry Fields', 'Leicester', 12938, 'F', 0);
-INSERT INTO customer VALUES (5552331, 'Paul', '15 Abbey Road', 'London', 56731, 'F', 0);
-INSERT INTO customer VALUES (5552511, 'Ringo', '251 Manchester Way', 'Liverpool', 85847, 'F', 0);
-INSERT INTO customer VALUES (5551342, 'George', '8 Maxwell Drive', 'Sussex', 47292, 'F', 0);
-INSERT INTO customer VALUES (5559705, 'Justin Timberlake', '20 Timberlake Rd', 'Babeville', 39483, 'F', 0);
-INSERT INTO customer VALUES (5550293, 'Will Smith', '10 Hollywood Blvd', 'Bel Air', 60594, 'F', 0);
-INSERT INTO customer VALUES (0405932, 'Nick Carter', '19 Venice Blvd', 'Santa Monica', 16049, 'F', 0);
-INSERT INTO customer VALUES (8392855, 'Aaron Carter', 'Aarons House', 'Los Angeles', 20405, 'F', 0);
-INSERT INTO customer VALUES (0897829, 'Hillary Duff', 'Aarons House', 'Los Angeles', 12345, 'F', 0);
-INSERT INTO customer VALUES (9705938, 'Lance Bass', '12 Washup Lane', 'New York', 48594, 'F', 0);
-INSERT INTO customer VALUES (2817594, 'Joey Fatone', '1515 NSync Drive', 'Nashville', 69504, 'F', 0);
-INSERT INTO customer VALUES (2039844, 'JC Chasez', '1515 NSync Drive', 'Nashville', 16958, 'F', 0);
+INSERT INTO customer VALUES (5551234, 'John', '222 Strawberry Fields', 'Leicester', 12938);
+INSERT INTO customer VALUES (5552331, 'Paul', '15 Abbey Road', 'London', 56731);
+INSERT INTO customer VALUES (5552511, 'Ringo', '251 Manchester Way', 'Liverpool', 85847);
+INSERT INTO customer VALUES (5551342, 'George', '8 Maxwell Drive', 'Sussex', 47292);
+INSERT INTO customer VALUES (5559705, 'Justin Timberlake', '20 Timberlake Rd', 'Babeville', 39483);
+INSERT INTO customer VALUES (5550293, 'Will Smith', '10 Hollywood Blvd', 'Bel Air', 60594);
+INSERT INTO customer VALUES (0405932, 'Nick Carter', '19 Venice Blvd', 'Santa Monica', 16049);
+INSERT INTO customer VALUES (8392855, 'Aaron Carter', 'Aarons House', 'Los Angeles', 20405);
+INSERT INTO customer VALUES (0897829, 'Hillary Duff', 'Aarons House', 'Los Angeles', 12345);
+INSERT INTO customer VALUES (9705938, 'Lance Bass', '12 Washup Lane', 'New York', 48594);
+INSERT INTO customer VALUES (2817594, 'Joey Fatone', '1515 NSync Drive', 'Nashville', 69504);
+INSERT INTO customer VALUES (2039844, 'JC Chasez', '1515 NSync Drive', 'Nashville', 16958);
 
 /* vehicle types */
 INSERT INTO vehicletype VALUES('Truck',NULL,600,200,45,50,20,5,50);
